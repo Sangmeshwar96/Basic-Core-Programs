a601deb687b03595a6abe95ad085c1e61397fccf
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-01 08:32:43 +0530

    Power Of Two Problem

---
+++ powerof2.cs
@@ -0,0 +1,14 @@
+﻿namespace PowerOf2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int value = 2;
+            for (int i = 0; i <= 31; i++)
+            {
+                Console.WriteLine($"{value}^{i} = {(long)Math.Pow(value, i):N0}");
+            }
+        }
+    }
+}

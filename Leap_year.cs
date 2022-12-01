8870c767d969e13ef0db67622e132a0007892866
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-01 08:31:07 +0530

    Leap Year Program

---
+++ Leap_year.cs
@@ -0,0 +1,28 @@
+﻿namespace Leap_year
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int year;
+
+            Console.WriteLine("Enter The Year ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
+            {
+                Console.WriteLine("The " + year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("The " + year + " is not a leap year");
+            }
+        
+            
+
+
+        }
+    }
+}
+
+

13110c03a0afa0158c8ed81ebb7e79c8cf1df951
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-10 21:51:17 +0530

    [Sangmeshwar] Add: Checking Leap Year

---
+++ Leap year/Leap year/Leap_year.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Leap_year_Program
+{
+    class Leap_year
+    {
+        public static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter The Year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
+            {
+                Console.WriteLine("The year " + year + " is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine("The year " + year + " is not a leap year.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+

19e1286cf5c56daef1691f92c9edfafda616fef5
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-13 21:28:17 +0530

    [Sangmeshwar] Add:  Checking Alphabet is vowel or consonent

---
+++ VowelOrConsonent/VowelOrConsonent/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VowelOrConsonent
+{
+    class VowelOrConsonent
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Alphabet A-Z or a-z :");
+            char ch = Convert.ToChar(Console.ReadLine().ToLower());
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                case 'e':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                case 'i':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                case 'o':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                case 'u':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                default:
+                    Console.WriteLine("{0} is Consonent", ch);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}

d6ae84ed4bbba7362e09c75e952b1bb01b000088
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-13 21:09:34 +0530

    [Sangmeshwar] Add:  Checking Number is Even Or Odd

---
+++ EvenAndOdd/EvenAndOdd/EvenAndOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EvenAndOdd
+{
+    class EvenAndOdd
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("--Welcome To Even Or Odd Number Program--");
+            Console.WriteLine("Enter a Number to find it is Even or Odd : ");
+            int num = Int32.Parse(Console.ReadLine());
+            int rem;
+            rem = num % 2;
+            if (rem == 0)
+            {
+                Console.WriteLine("{0} Entered Number is Even", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} Entered Number is odd", num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
